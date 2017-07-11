98eb7c9cfd0f0a62f92a922f20362489730919f5
Samuel Carrara <samuel@redecredauto.com.br>
2017-07-11 16:52:10 -0300

    Removed Json Class

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php
-	if(!function_exists('json_encode')) @include('json.php');
-	header("Content-Type: application/json", true);
 	require_once('Youtube.class.php');
-	$youtube = new Youtube;
-	$youtube->apikey = 'AIzaSyAtX4uCTbyn9JJP31Hm_PEN4Y-MxhY7ivw';
-	$youtube->playlistid =  'PLrW9pOMXGA7jfSjqdzGnG-POSVwdyLhqe';
+	$youtube = new Youtube;			
 ?>
