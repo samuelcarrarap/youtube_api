a347176e1806d5d838107b8cc506a481ab783ae3
Samuel Carrara <samuel@redecredauto.com.br>
2017-07-11 17:16:18 -0300

    Removed the index file

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-	require_once('Youtube.class.php');
-	$youtube = new Youtube;	
-	$youtube->apikey = "AIzaSyAtX4uCTbyn9JJP31Hm_PEN4Y-MxhY7ivw";
-	$youtube->channelid = "AIzaSyAtX4uCTbyn9JJP31Hm_PEN4Y-MxhY7ivw";
-	print_r($youtube->ChannelVideos());
-?>
